af26bd57643610fed511987972000a8dd7b1ed11
TypicalMotion <51135683+TypicalMotion@users.noreply.github.com>
2019-08-17 14:21:56 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ particlesJS('particles-js',
         }
       },
       "size": {
-        "value": 5,
+        "value": 3,
         "random": true,
         "anim": {
           "enable": false,
@@ -69,7 +69,7 @@ particlesJS('particles-js',
       },
       "move": {
         "enable": true,
-        "speed": 4,
+        "speed": 3,
         "direction": "none",
         "random": false,
         "straight": false,
